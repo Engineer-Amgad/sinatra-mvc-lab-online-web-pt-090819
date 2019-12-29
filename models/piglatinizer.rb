9fafbf66d0e07b8e1a53c9f5f92cd4323d6f5b6c
Amgad Malik <amgadmalik@yahoo.com>
2019-12-29 17:03:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,17 @@
+class PigLatinizer
+  def translate(str)
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
 
-def translate(str)
-  alpha = ('a'..'z').to_a
-  vowels = %w[a e i o u]
-  consonants = alpha - vowels
-
-  if vowels.include?(str[0])
-    str + 'way'
-  elsif consonants.include?(str[0]) && consonants.include?(str[1])
-    str[2..-1] + str[0..1] + 'ay'
-  elsif consonants.include?(str[0])
-    str[1..-1] + str[0] + 'ay'
-  else
-    str # return unchanged
+    if vowels.include?(str[0])
+      str + 'way'
+    elsif consonants.include?(str[0]) && consonants.include?(str[1])
+      str[2..-1] + str[0..1] + 'ay'
+    elsif consonants.include?(str[0])
+      str[1..-1] + str[0] + 'ay'
+    else
+      str # return unchanged
+    end
   end
-end
+end 
