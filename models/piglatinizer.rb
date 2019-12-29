5f4100913aea46404aa330197237c27bcdf8dc3f
Amgad Malik <amgadmalik@yahoo.com>
2019-12-29 17:47:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,9 +11,8 @@ class PigLatinizer
     vowels = %w[a e i o u]
     consonants = alpha - vowels
 
-    if vowels.include?(str[0])
-      piglatinized_str = str + 'way'
-      piglatinized_str.capitalize
+    if vowels.include?(str[0].)
+      str + 'way'
     elsif consonants.include?(str[0]) && consonants.include?(str[1])
       str[2..-1] + str[0..1] + 'ay'
     elsif consonants.include?(str[0])
